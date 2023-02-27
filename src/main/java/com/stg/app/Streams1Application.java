40d3ccd7e3a7855dbbb90103bacb7e6b66b83d24
haribhaskarasain <haribhaskarasain@stgit.com>
2023-02-27 11:57:03 +0530

    first commit

---
+++ src/main/java/com/stg/app/Streams1Application.java
@@ -0,0 +1,13 @@
+package com.stg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streams1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Streams1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stg/Streams1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Streams1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
